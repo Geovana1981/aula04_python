72a29ea99076ae30565121460e056e65e95b3fab
Geovana1981 <geovana.nogueira@aluno.senac.rio>
2025-11-25 21:24:52 -0300

    Operadores And e Or

--- Atividades.py
+++ Atividades.py
@@ -1,9 +1,10 @@
-valor = float(input("Informe o valor da sua compra:"))
+#
+ valor = float(input("Informe o valor da sua compra:"))
 
-if valor  >= 250.00:
-    desconto = valor * 0.16
-    total = valor - desconto
-    print("Parabéns! Com o seu valor gasto, o desconto de 16% é aplicável. Sendo assim, o total a pagar é de R$", total) 
+# if valor  >= 250.00:
+#     desconto = valor * 0.16
+#     total = valor - desconto
+#     print("Parabéns! Com o seu valor gasto, o desconto de 16% é aplicável. Sendo assim, o total a pagar é de R$", total) 
 
-else:
-    print("Infelizmente, com o seu valor gasto, o desconto de 16% não é aplicável. Sendo assim, o total a pagar continua sendo de R$", valor)
+# else:
+#     print("Infelizmente, com o seu valor gasto, o desconto de 16% não é aplicável. Sendo assim, o total a pagar continua sendo de R$", valor)
--- aula04/exemplo1.py
+++ aula04/exemplo1.py
@@ -1,3 +1,5 @@
+# Verificação de Idade
+
 # idade = int(input("Informe sua idade:"))
 
 # if idade  >= 18:
@@ -7,6 +9,7 @@
 #     print("Você é menor de idade.") 
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------#
+# Nivelamento com pontos
 
 # pontos = int(input("Quantos pontos? "))
 
@@ -23,4 +26,41 @@
 #     print("Pontuação Baixa. Por favor, tente novamente.")
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------#
+# Verificação de Login
 
+# usuario = input("Informe o usuário:").lower()
+# senha = input("Digite a senha:")
+
+# print(usuario)
+
+# if usuario == 'admin' and senha == '1234':
+#     print("Login concluído sucesso")
+
+# else:
+#     print("O usuário e/ou a senha estão incorretos. Tente novamente.")
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------#
+# Condição para brinde
+
+# compra = float(input("Valor da compra:"))
+# cliente = input("Você é um cliente frequente? [S/N] ").lower()
+
+# if compra >= 1000 or cliente == "s":
+#     print("Parabéns, você está apto à receber o brinde!")
+
+# else:
+#     print("Que pena, você não está apto à receber o brinde.")
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------#
+#Aprovação (Nota/Frequência)
+
+nota = float(input("Informe sua nota:"))
+frequencia = float(input("Informe sua frequência:"))
+
+if nota >= 7:
+    if frequencia >= 75:
+        print("Aprovado(a)!")
+    else:
+        print("Boa nota, porém está reprovado(a) por falta.")
+else:
+    print("Reprovado (a) por nota.")
